1e731ea1e91749dc80883698a947025c1d0c44e6
Dheemahi9 <viswanadhapallinagalakshmi2@gmail.com>
2024-11-07 16:23:30 +0530

    Add files via upload

---
+++ DAY-1/Employee.java
@@ -0,0 +1,12 @@
+class Employee
+{
+public static void main(String args[])
+{
+int eno=101;
+String ename="Mahi";
+float ft=544.45f;
+System.out.println("the given employee number is:"+eno);
+System.out.println("the given employee name is:"+ename);
+System.out.println("the given employee salary is:"+ft);
+}
+}
---
+++ DAY-1/First.java
@@ -0,0 +1,14 @@
+//sample programme
+/*
+sample
+java
+programme
+*/
+import java.lang.*;
+class First
+{
+public static void main(String args[])
+{
+System.out.println("MY NAME IS DHEEMAHI");
+}
+}
